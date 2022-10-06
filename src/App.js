f21c001a705acd7817b1ea88cbaed46649822346
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-10-06 09:08:11 -0500

    initial react file structure modified for this project, test render working

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="test">THIS IS A TEST!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
