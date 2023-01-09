eb76bf51bdfa48cd15acfe5a30b727ca2b137e9d
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2023-01-08 22:20:26 -0600

    changed picture/headshot

--- src/components/Contactform/index.js
+++ src/components/Contactform/index.js
@@ -1,6 +1,8 @@
+//formState is not setting correctly, email doesn't use formState.name .message to fill the email ??
+
 import React, { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 const ContactForm = (props) => {
   const [formState, setFormState] = useState({
@@ -34,54 +36,62 @@ const ContactForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormState({
+      name: { name },
+      email: { email },
+      message: { message },
+    });
+    console.log(formState);
     window.location = `mailto:gribbler25@gmail.com?subject=message from ${formState.name}&body=${formState.message}`;
-    // console.log(formState);
-    // setFormState({ name: "", email: "", message: "" }); //doesn't appear to be clearing the form.
-    //document.getElementById("contact-form").reset();  this isn't working either...
   };
 
   return (
     <Container id="contact" className="contact">
       <h1>Contact Me</h1>
-      <Container className="bg-light mb-4 rounded-3">
-        <form id="contact-form" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              defaultValue={name}
-              onBlur={handleChange}
-            />
-          </div>
+      <Form
+        id="contact-form"
+        className="bg-light mb-4 rounded-3"
+        onSubmit={handleSubmit}
+      >
+        <div>
+          <Form.Label>Name:</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
 
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              defaultValue={email}
-              onBlur={handleChange}
-            />
-          </div>
+        <div>
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="enter email"
+            onBlur={handleChange}
+            defaultValue={email}
+          />
+        </div>
 
+        <div>
+          <Form.Label>Message:</Form.Label>
+          <Form.Control
+            type="text"
+            name="message"
+            onBlur={handleChange}
+            defaultValue={message}
+          />
+        </div>
+        {errorMessage && (
           <div>
-            <label htmlFor="message">Message:</label>
-            <textarea
-              name="message"
-              row="5"
-              defaultValue={message}
-              onBlur={handleChange}
-            />
+            <p className="error-text">{errorMessage}</p>
           </div>
-          {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
-            </div>
-          )}
-          <Button type="submit">Submit!</Button>
-        </form>
-      </Container>
+        )}
+        <Button varient="primary" type="submit">
+          Submit!
+        </Button>
+      </Form>
     </Container>
   );
 };
